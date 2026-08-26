82e43d63ef0141bdadfa7c0120fb39808c9fdb16
kilianar <mail@kilianar.de>
2026-08-26 14:08:57 +0200

    feat(convexity): finish proof on cartesian product of convex hulls

--- Polyhedral/Mathlib/Geometry/Convex/ConvexSpace/Set/Hull.lean
+++ Polyhedral/Mathlib/Geometry/Convex/ConvexSpace/Set/Hull.lean
@@ -20,19 +20,17 @@ convex set containing `s`. In order theory speak, this is a closure operator.
 public section
 
 open Set
-
 namespace Convexity
-
-
 section Pointwise
-
 
 open Pointwise
 
-variable {R X Y V A : Type*}
+variable {R V A X Y : Type*}
+
 variable [Ring R] [PartialOrder R] [IsStrictOrderedRing R]
-variable [ConvexSpace R X] [ConvexSpace R Y]
 variable [AddCommGroup V] [Module R V] [ConvexSpace R V] [IsModuleConvexSpace R V]
+variable [ConvexSpace R X]
+variable [ConvexSpace R Y]
 
 @[simp] lemma convexHull_neg (s : Set V) : -convexHull R s = convexHull R (-s) := by
   ext x
@@ -46,48 +44,42 @@ variable [AddTorsor V A] [ConvexSpace R A] [IsAffineConvexSpace R V A]
 lemma convexHull_vadd (s₁ : Set V) (s₂ : Set A) :
     convexHull R (s₁ +ᵥ s₂) = convexHull R s₁ +ᵥ convexHull R s₂ := by
   apply le_antisymm
-  · apply convexHull_min
+  · refine convexHull_min ?_ ?_
     · exact vadd_subset_vadd subset_convexHull_self <| subset_convexHull_self
-    apply IsConvexSet.vadd IsConvexSet.convexHull <| IsConvexSet.convexHull
-  · sorry
-
-lemma convexHull_prod_singleton_left (x : X) (s₂ : Set Y) :
-    convexHull R ({x} ×ˢ s₂) = {x} ×ˢ convexHull R s₂ := by
     sorry
+  sorry
 
-lemma convexHull_prod_singleton_right (s₁ : Set X) (y : Y) :
-    convexHull R (s₁ ×ˢ {y}) = convexHull R s₁ ×ˢ {y} := by
-    sorry
-
-lemma convexHull_prod (s₁ : Set X) (s₂ : Set Y) :
-    convexHull R (s₁ ×ˢ s₂) = (convexHull R s₁) ×ˢ (convexHull R s₂) := by
-  apply le_antisymm
-  · apply convexHull_min
-    · exact prod_mono subset_convexHull_self subset_convexHull_self
+lemma convexHull_prod (s : Set X) (t : Set Y) :
+  convexHull R (s ×ˢ t) = (convexHull R s) ×ˢ (convexHull R t) := by
+  apply Set.Subset.antisymm
+  · refine convexHull_min
+      (prod_mono subset_convexHull_self subset_convexHull_self) ?_
     exact IsConvexSet.convexHull.prod IsConvexSet.convexHull
-  · have h : s₁ ×ˢ (convexHull R s₂) ⊆ convexHull R (s₁ ×ˢ s₂) := by
-      refine prod_subset_iff.mpr ?_
-      intro x hx y hy
-      have h₃ : {x} ×ˢ (convexHull R s₂) ⊆ convexHull R (s₁ ×ˢ s₂) := by
-        rewrite [← convexHull_prod_singleton_left]
-        apply convexHull_mono
-        apply prod_mono_left
-        exact singleton_subset_iff.mpr hx
-      apply h₃
-      simp [hy]
-    refine prod_subset_iff.mpr ?_
-    intro x hx y hy
-    have h₂ : s₁ ×ˢ {y} ⊆ convexHull R (s₁ ×ˢ s₂) := by
-      apply le_trans _ h
-      apply prod_mono_right
-      exact singleton_subset_iff.mpr hy
-    have h₁ : convexHull R s₁ ×ˢ {y} ⊆ convexHull R (s₁ ×ˢ s₂):= by
-      rw [← convexHull_prod_singleton_right]
-      nth_rw 2 [← convexHull_eq_self.mpr IsConvexSet.convexHull]
-      apply convexHull_mono
-      exact h₂
-    apply h₁
-    simp [hx]
+  · rintro ⟨x, y⟩ ⟨hx, hy⟩
+    have hX (y₀ : Y) : IsAffineMap R (fun x₀ : X ↦ (x₀, y₀)) := by
+      constructor
+      intro w
+      ext <;> simp
+    have hY (x₀ : X) :
+        IsAffineMap R (fun y₀ : Y ↦ (x₀, y₀)) := by
+      constructor
+      intro w
+      ext <;> simp
+    have hx_prod (y₀ : Y) (hy₀ : y₀ ∈ t) :
+        (x, y₀) ∈ convexHull R (s ×ˢ t) := by
+      refine convexHull_mono (R := R)
+        (s := (fun x₀ : X ↦ (x₀, y₀)) '' s) ?_ ?_
+      · rintro _ ⟨x₀, hx₀, rfl⟩
+        exact ⟨hx₀, hy₀⟩
+      · rw [← (hX y₀).image_convexHull s]
+        exact ⟨x, hx, rfl⟩
+    rw [← (IsConvexSet.convexHull (R := R) (s := s ×ˢ t)).convexHull_eq_self]
+    refine convexHull_mono (R := R)
+      (s := (fun y' : Y ↦ (x, y')) '' t) ?_ ?_
+    · rintro _ ⟨y', hy', rfl⟩
+      exact hx_prod y' hy'
+    · rw [← (hY x).image_convexHull t]
+      exact ⟨y, hy, rfl⟩
 
 end Pointwise
 
