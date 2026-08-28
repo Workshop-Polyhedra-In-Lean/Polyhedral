dca2508e9011ef7039129ee97a8552c8f7a9b14b
John Maar <johntylermaar@gmail.com>
2026-08-28 14:31:52 +0200

    feat: reduced UnitCube_IsPolytope to a statement on convexHull of Set.pi

--- Polyhedral/Mathlib/Geometry/Convex/ConvexSpace/Polytope/Zonotope.lean
+++ Polyhedral/Mathlib/Geometry/Convex/ConvexSpace/Polytope/Zonotope.lean
@@ -25,18 +25,23 @@ def isZonotope (s : Set X) := ∃ segs : Multiset (Set X),
 variable (R) in
 def UnitCube (n : ℕ) := Set.pi ⊤ (fun (_ : Fin n) => convexHull R {(0 : R), (1 : R)})
 
+--TODO : generalize this to families of convex spaces
+lemma convexHull_pi {ι : Type*} [Finite ι] (s : Set ι) (α : ι → Set X) :
+    Set.pi s (fun i => convexHull R (α i)) = convexHull R (Set.pi s α) := by
+  have hs : Fintype s := Fintype.ofFinite ↑s
+  rw [← Set.coe_toFinset s]
+  set t := s.toFinset
+  induction t using Finset.induction
+  · simp
+  · sorry --this might be hard, since we only proved convexHull distributing over cartesian prodúct but this is an intersection.
+
 lemma UnitCube_IsPolytope (n : ℕ) : IsPolytope R (UnitCube R n) := by
   let t := Set.pi ⊤ (fun (_ : Fin n) => {(0 : R), (1 : R)})
   have hfinite : t.Finite := by
     refine Set.Finite.pi fun i => ?_
     simp only [Set.finite_insert, Set.finite_singleton]
-  have ht : UnitCube R n = convexHull R t := by
-    ext x
-    constructor
-    · sorry
-    · sorry
+  have ht : UnitCube R n = convexHull R t := convexHull_pi _ _
   exact ht ▸ IsPolytope.convexHull_finite R hfinite
-
 
 end Semiring
 
