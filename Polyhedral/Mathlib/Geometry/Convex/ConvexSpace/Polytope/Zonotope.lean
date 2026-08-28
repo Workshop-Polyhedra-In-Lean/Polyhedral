29b5376ca028b0ec7f1dec5acddd5ae4bd18b279
Matt Dupraz <matthew.dupraz@gmail.com>
2026-08-28 12:34:34 +0200

    Zonotope is a sum of line segments

--- Polyhedral/Mathlib/Geometry/Convex/ConvexSpace/Polytope/Zonotope.lean
+++ Polyhedral/Mathlib/Geometry/Convex/ConvexSpace/Polytope/Zonotope.lean
@@ -11,19 +11,17 @@ namespace Convexity
 
 section Semiring
 
-variable {R X : Type*}
+variable {R X V : Type*}
 variable [Ring R] [PartialOrder R] [IsStrictOrderedRing R]
-variable [ConvexSpace R X] [AddCommMonoid X]
+variable [ConvexSpace R X]
+variable [AddCommGroup V] [Module R V] [ConvexSpace R V] [IsModuleConvexSpace R V]
 
 open scoped Pointwise
 
-def IsLineSegment (s : Set X) := ∃ a, ∃ b≠a, s = convexHull R {a, b}
-
-def isZonotope (s : Set X) := ∃ segs : Multiset (Set X),
-  (∀ seg ∈ segs, IsLineSegment (R := R) (seg)) ∧ (segs.sum = s)
-
 variable (R) in
 def UnitCube (n : ℕ) := Set.pi ⊤ (fun (_ : Fin n) => convexHull R {(0 : R), (1 : R)})
+
+
 
 lemma UnitCube_IsPolytope (n : ℕ) : IsPolytope R (UnitCube R n) := by
   let t := Set.pi ⊤ (fun (_ : Fin n) => {(0 : R), (1 : R)})
@@ -36,6 +34,31 @@ lemma UnitCube_IsPolytope (n : ℕ) : IsPolytope R (UnitCube R n) := by
     · sorry
     · sorry
   exact ht ▸ IsPolytope.convexHull_finite R hfinite
+
+variable (R) in
+def IsLineSegment (s : Set X) := ∃ a, ∃ b≠a, s = convexHull R {a, b}
+
+variable (R) in
+def isZonotope (s : Set X) := ∃ n : ℕ, ∃ f : (Fin n → R) → X,
+   IsAffineMap R f ∧ f '' UnitCube (R := R) n = s
+
+variable (R) in
+lemma sumOfSegments_isZonotope (s : Set V) :
+  (∃ segs : Multiset (Set V),
+    (∀ seg ∈ segs, IsLineSegment R (seg)) ∧ (segs.sum = s)) → isZonotope R s := sorry
+
+variable (R) in
+lemma zonotope_isSumOfSegments (s : Set V) :
+  isZonotope R s → (∃ segs : Multiset (Set V),
+    (∀ seg ∈ segs, IsLineSegment R (seg)) ∧ (segs.sum = s)) := sorry
+
+variable (R) in
+lemma zonotope_iff_sumOfSegments (s : Set V) :
+  isZonotope R s ↔ (∃ segs : Multiset (Set V),
+    (∀ seg ∈ segs, IsLineSegment R (seg)) ∧ (segs.sum = s)) := by
+  constructor
+  · exact zonotope_isSumOfSegments R s
+  · exact sumOfSegments_isZonotope R s
 
 
 end Semiring
