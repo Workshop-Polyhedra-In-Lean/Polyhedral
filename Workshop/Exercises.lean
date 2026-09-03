0e6b9f21e3f4c618165ae96e47c612ba1dfd214c
Günter Rote <rote@inf.fu-berlin.de>
2026-09-03 22:22:56 +0200

    made a calc proof

--- Workshop/Exercises.lean
+++ Workshop/Exercises.lean
@@ -18,7 +18,8 @@ def IsPolytope (s : Set X) : Prop := ∃ t : Finset X, s = Convexity.convexHull 
 Given the definition of a polytope, show that the empty set is a polytope. -/
 
 lemma empty_isPolytope : IsPolytope R (∅ : Set X) := by
-  sorry
+  use ∅
+  simp only [Finset.coe_empty, Convexity.convexHull_empty]
 
 /- # Exercise 2:
 Define the unit square as the convex hull of its vertices in the convex space
@@ -26,40 +27,54 @@ and show that is a polytope. -/
 
 def vertices : Finset (ℚ × ℚ) := {(0, 0), (1,0), (0,1), (1,1)}
 
-def unitSquare : Set (ℚ × ℚ) := sorry
+def unitSquare : Set (ℚ × ℚ) := Convexity.convexHull ℚ vertices
 
 theorem unitSquare_isPolytope : IsPolytope ℚ unitSquare := by
-  sorry
+  use vertices
+  rw [unitSquare]
 
 /- # Exercise 3:
 Show that polytopes are convex sets -/
 
 lemma polytope_isConvexSet (hP : IsPolytope R P) : IsConvexSet R P := by
-  sorry
+  let ⟨ t, ht ⟩ := hP
+  rw [ht]
+  apply IsConvexSet.convexHull
 
-/- Constructor is a tactic that we use to split a complex goal into multiple simpler
+/- `constructor is a tactic that we use to split a complex goal into multiple simpler
 goals. To prove that a convex set is a face of another we need to show two things:
 the first one is a subset relation and the second one is that is the extreme property. -/
 
 /- # Exercise 4:
 Show that every convex set is a face of itself -/
 theorem refl_convexSet (S : ConvexSet R X) : S.IsFaceOf S := by
-  sorry
+  constructor
+  · simp only [Std.le_refl]
+  · tauto
 
 /- # Exercise 5:
 Show that the intersection of two convex sets is a convex set -/
 
 lemma intersection_isConvexSet (hP₁ : IsConvexSet R P₁) (hP₂ : IsConvexSet R P₂) :
     IsConvexSet R (P₁ ∩ P₂) := by
-  sorry
+  intro w a
+  simp only [Set.mem_inter_iff]
+  simp at a
+  have a1 : ↑w.weights.support ⊆ P₁ := by tauto
+  have a2 : ↑w.weights.support ⊆ P₂ := by tauto
+  apply hP₁ at a1
+  apply hP₂ at a2
+  tauto
+
 
 /- # Exercise 6:
 Show that the convex hull of the union of finitely many polytopes is a polytope.
 For this we can start by showing that the convex hull of the union of two polytopes is a
-polytope and then use the iduction tactic. -/
+polytope and then use the induction tactic. -/
 
 lemma convexHull_union (h₁ : IsPolytope R P₁) (h₂ : IsPolytope R P₂) :
     IsPolytope R (convexHull R (P₁ ∪ P₂)) := by
+  intro
   sorry
 
 lemma convexHull_iUnion_finite {p : Set (Set X)} (hp : p.Finite)
